3c3b420412466fd2da734e93a616e8ced5ce49f1
Roman <90706077+V1rs999@users.noreply.github.com>
2023-12-01 17:21:19 +0200

    Delete WebApi directory

--- WebApi/WebApi/Dto/ErrorsDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApi.Dto
-{
-    public class ErrorsDto
-    {
-        public string Code { get; set; }
-        public string Description { get; set; }
-    }
-}
--- WebApi/WebApi/Dto/Response.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApi.Dto
-{
-    public class Response
-    {
-        public string? Status { get; set; }
-        public string? Message { get; set; }
-    }
-}
