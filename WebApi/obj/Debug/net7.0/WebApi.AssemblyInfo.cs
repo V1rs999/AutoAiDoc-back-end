a846e12d27d764e45d6033103fa263e697983dfb
Roman <romadraguca@gmail.com>
2024-02-01 19:42:52 +0200

    test deploy

--- WebApi/obj/Debug/net7.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net7.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da7339e07e3ab2661ee43a0c0a368c14f6488ffe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
